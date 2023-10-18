d80ff7400a4bf855e5e014842040a1dc5cff9d0a
Kshitij Prabhu <114593715+prabhu-ks@users.noreply.github.com>
2023-10-18 01:57:38 -0400

    Packer Commit-27

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+# DATABASE_USER="${{'secrets.DATABASE_USERNAME'}}"
+# DATABASE_PASS="$secrets.DATABASE_PASSWORD"
+# DATABASE="$secrets.DATABASE_NAME"
+# DATABASE_HOST="$secrets.DATABASE_HOST"
+# DATABASE_PORT="$secrets.DATABASE_PORT"
+
+
 sudo apt update
 
 sudo apt upgrade -y
@@ -10,17 +17,15 @@ sudo "CHECKPOINT_DISABLE=1"
 
 sudo apt install -y nodejs npm postgresql
 
-sudo -u postgres psql -c "ALTER USER $DATABASE_USER WITH PASSWORD $DATABASE_PASS;"
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'root1234';"
           
-PGPASSWORD="$DATABASE_PASS" psql -U "$DATABASE_USER" -d "$DATABASE" -h "$DATABASE_HOST"-p "$DATABASE_PORT"
+PGPASSWORD="root1234" psql -U "postgres" -h "127.0.0.1" -p 5432 
 
 sudo apt install unzip
 
-cd /tmp
+unzip webapp.zip -d webapp
 
-unzip webapp.zip -d /tmp/assignment5
-
-cd /tmp/assignment5/webapp
+cd webapp
 
 sudo npm i
 
