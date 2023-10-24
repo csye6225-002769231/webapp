ba18b8507e8ee6fff76f0721b49e6e584fe49541
Kshitij Prabhu <114593715+prabhu-ks@users.noreply.github.com>
2023-10-24 01:04:21 -0400

    systemd commit

--- setup.sh
+++ setup.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
-# DATABASE_USER="${{'secrets.DATABASE_USERNAME'}}"
-# DATABASE_PASS="$secrets.DATABASE_PASSWORD"
-# DATABASE="$secrets.DATABASE_NAME"
-# DATABASE_HOST="$secrets.DATABASE_HOST"
-# DATABASE_PORT="$secrets.DATABASE_PORT"
-
-
 sudo apt update
 
 sudo apt upgrade -y
@@ -15,11 +8,7 @@ sudo "DEBIAN_FRONTEND = noninteractive"
 
 sudo "CHECKPOINT_DISABLE=1"
 
-sudo apt install -y nodejs npm postgresql
-
-sudo -u postgres psql -c "ALTER USER $DATABASE_USER WITH PASSWORD '$DATABASE_PASS';"
-          
-PGPASSWORD="$DATABASE_PASS" psql -U "$DATABASE_USER" -h "$DATABASE_HOST" -p "$DATABASE_PORT"
+sudo apt install -y nodejs npm
 
 sudo groupadd users-csye6225
 
