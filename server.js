3f8fd71b6d3a7f5648c6cdf2a188245494a5765d
Kshitij Prabhu <114593715+prabhu-ks@users.noreply.github.com>
2023-10-25 17:35:27 -0400

    systemd commit-11

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // const csv = require('./csvparser.js');
 const basicAuth = require("./middleware/auth.js");
-const assignment_controllers=require("./controllers/assignment_controllers.js")
+const assignment_controllers = require("./controllers/assignment_controllers.js")
 
 // csv.importData()
 //   .then(() => {
@@ -25,65 +25,63 @@ const assignment_controllers=require("./controllers/assignment_controllers.js")
 
 
 app.all('/healthz', async (req, res) => {
-   res.set('Cache-control', 'no-cache');
- 
-   try {
-     // Attempt to connect to the database asynchronously
-     const val = await database.conn;
-     
-     const bodyLength = parseInt(req.get('Content-Length') || '0', 10);
- 
-     // This will run only if the request is GET
-     if (req.method === 'GET') {
-       // Checking for body and query lengths
-       if (Object.keys(req.query).length > 0 || bodyLength > 0) {
-         res.status(400).send(); // Bad request
-       } else {
-         if (!val) {
-           res.status(503).send(); // Not connected
-         } else {
-           res.status(200).send(); // Connected
-         }
-       }
-     } else {
-       res.status(405).send(); // Method not allowed (except GET)
-     }
-   } catch (error) {
-     // Handle the database connection error here
-     console.error('Database connection error:', error);
-     res.status(503).send(); // Database connection error
-   }
- });
+  res.set('Cache-control', 'no-cache');
+
+  try {
+
+
+    const bodyLength = parseInt(req.get('Content-Length') || '0', 10);
+    if (req.method === 'GET') {
+      // Checking for body and query lengths
+      if (Object.keys(req.query).length > 0 || bodyLength > 0) {
+        res.status(400).send(); // Bad request
+      } else {
+        const val = await database.conn();
+        if (!val) {
+          res.status(503).send(); // Not connected
+        } else {
+          res.status(200).send(); // Connected
+        }
+      }
+    } else {
+      res.status(405).send(); // Method not allowed (except GET)
+    }
+  } catch (error) {
+    // Handle the database connection error here
+    console.error('Database connection error:', error);
+    res.status(503).send(); // Database connection error
+  }
+});
 
 app.use(express.json())
 
-app.post('/v1/assignments', basicAuth.basicAuth,assignment_controllers.createAssignment);
+app.post('/v1/assignments', basicAuth.basicAuth, assignment_controllers.createAssignment);
 app.post('/v1/assignments/*', (req, res) => {
-   res.status(400).send(); 
+  res.status(400).send();
 });
-app.get('/v1/assignments', basicAuth.basicAuth,assignment_controllers.getAllAssignments);
-app.get('/v1/assignments/:id',basicAuth.basicAuth, assignment_controllers.getAssignmentById);
-app.put('/v1/assignments/:id',basicAuth.basicAuth, assignment_controllers.updateAssignment);
-app.delete('/v1/assignments/:id',basicAuth.basicAuth, assignment_controllers.deleteAssignment);
+app.get('/v1/assignments', basicAuth.basicAuth, assignment_controllers.getAllAssignments);
+app.get('/v1/assignments/:id', basicAuth.basicAuth, assignment_controllers.getAssignmentById);
+app.put('/v1/assignments/:id', basicAuth.basicAuth, assignment_controllers.updateAssignment);
+app.delete('/v1/assignments/:id', basicAuth.basicAuth, assignment_controllers.deleteAssignment);
 
 app.all('/*', (req, res) => {
-   if (req.method === 'PATCH' || req.method === 'HEAD' || req.method === 'OPTIONS') {
-      res.status(405).send();
-   } else {
-      res.status(404).send(); // Not found or wrong link
-   }
+  if (req.method === 'PATCH' || req.method === 'HEAD' || req.method === 'OPTIONS') {
+    res.status(405).send();
+  } else {
+    res.status(404).send(); // Not found or wrong link
+  }
 });
 
 
 app.listen(3000, (err) => {
-   if (err) throw err;
-   else{
-      console.log(`Server is running on port `);
-   }
+  if (err) throw err;
+  else {
+    console.log(`Server is running on port `);
+  }
 });
 
-module.exports={
-   app
+module.exports = {
+  app
 }
 
 
