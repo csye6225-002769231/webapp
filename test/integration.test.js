67c147f72066ecf8d43597b37d76d27f9b80d5af
Kshitij Prabhu <114593715+prabhu-ks@users.noreply.github.com>
2023-10-25 20:26:28 -0400

    systemd commit-25

--- test/integration.test.js
+++ test/integration.test.js
@@ -2,7 +2,7 @@ const db = require("../db.js").bootstrapDatabase;
 const { describe } = require('mocha')
 const chai = require('chai')
 const chaiHttp = require('chai-http');
-const app = require('../server.js');  // Adjust the path to your app's entry point
+const app = require('../server.js').app;  // Adjust the path to your app's entry point
 
 const { expect } = chai;
 
@@ -45,8 +45,9 @@ chai.use(chaiHttp);
 describe("CI Testing for GET/healthz", () => {
     it("Successfully check the Db connection", async () => {
       let dbstatus = true;
-      db()
+  
       try {
+  
         const response = await chai.request(app).get("/healthz");
         expect(response).to.have.status(200);
   
@@ -54,9 +55,10 @@ describe("CI Testing for GET/healthz", () => {
         console.error("Test Error:", error);
         dbstatus = false;
         expect(dbstatus, "Database connection failed").to.be.true;
-      } 
+      } finally {
+        setTimeout(() => {
+          process.exit(0); 
+        }, 2000); // 
+      }
     });
-    setTimeout(() => {
-      process.exit(0);
-      }, 2000)
-  }); 
+  });
